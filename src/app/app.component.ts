aab4eaa6a7b7d838f9208704724cf07372677fef
Samuel Macedo <samuel.macedo@mxm.com.br>
2025-03-10 14:32:45 -0300

    cfg amplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+   console.log(Amplify.getConfig());
+  }
   title = 'mx-amplify-1';
 }
