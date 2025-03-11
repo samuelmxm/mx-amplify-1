c3bf578b17ae3672e597a76bda78ec717847eb79
Samuel Macedo <samuel.macedo@mxm.com.br>
2025-03-11 08:28:39 -0300

    variaveis ambiente

--- amplify/functions/hello/resource.ts
+++ amplify/functions/hello/resource.ts
@@ -1,9 +1,16 @@
 import { defineFunction } from "@aws-amplify/backend";
 
+export const REGION = "sa-east-1";
+export const TABLE_NAME = "PRODUCTS";
+
 export const sayHello = defineFunction({
     name: 'sayHello',
     entry: './handler.ts',
     bundling: {
         minify: false
+    },
+    environment: {
+        REGION,
+        TABLE_NAME
     }
 });
