33b49ffb10df639e991481f3ff53921607b9549b
Samuel Macedo <samuel.macedo@mxm.com.br>
2025-03-12 11:52:13 -0300

    ajuste

--- amplify/functions/teste/handler.ts
+++ amplify/functions/teste/handler.ts
@@ -1,12 +1,13 @@
 import type { Handler } from 'aws-lambda';
 import { Schema } from '../../data/resource';
 import { env } from '$amplify/env/teste'; //para gerar o objeto, necessário rodar o 'npx ampx sandbox'
-import {MongoClient} from 'mongodb';
+//import {MongoClient} from 'mongodb';
 //type handlerType = Schema['teste']['functionHandler']
 
-const client = new MongoClient(env.MONGOCN);
+//const client = new MongoClient(env.MONGOCN);
 
 export const handler: Handler = async (event: any, context: any) => {
+    /*
     const db = await client.db("analise_de_codigo");
     const collection = await db.collection("registro_dependency_check");
     const body = await collection.find().limit(10).toArray();
@@ -15,4 +16,6 @@ export const handler: Handler = async (event: any, context: any) => {
         body
     };
     return response;
+    */
+   return ': )';
 }
