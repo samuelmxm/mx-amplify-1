5bba197b0b4ada098e2319a305956af7aa3a656d
Samuel Macedo <samuel.macedo@mxm.com.br>
2025-03-12 16:59:58 -0300

    material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { generateClient } from 'aws-amplify/api';
 import { Schema } from '../../amplify/data/resource';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
-import { fetchUserAttributes } from 'aws-amplify/auth';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 Amplify.configure(outputs);
 
@@ -19,18 +21,27 @@ async function sayHello() {
   console.log('Result', result);
 }
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AmplifyAuthenticatorModule],
+  imports: [RouterOutlet, 
+    AmplifyAuthenticatorModule, 
+    
+    MatButtonModule, MatMenuModule, MatIconModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+
   constructor(public authenticator: AuthenticatorService) { }
 
   ngOnInit(): void {
     console.log(Amplify.getConfig());
+
+
 
     /*const userData = fetchUserAttributes().then(u => {
       this.nickname = u.nickname;
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InventarioComponentesMainComponent } from './telas/inventario-componentes-main/inventario-componentes-main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inventariodecomponentes', component: InventarioComponentesMainComponent },
+];
