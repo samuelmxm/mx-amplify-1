a76830b1c5821287056a6083db9f309d189dd97f
Samuel Macedo <samuel.macedo@mxm.com.br>
2025-03-13 10:06:25 -0300

    f

--- amplify/functions/teste/handler.ts
+++ amplify/functions/teste/handler.ts
@@ -8,5 +8,19 @@ const client = new MongoClient(env.MONGOCN);
 
 export const handler: Handler = async (event: any, context: any) => {
 
-   return '';
+    const campos = {
+        _id: 1,   // Incluir campo nome
+        DataRegistro: 1   // Incluir campo email
+      };
+
+    const db = await client.db("analise_de_codigo");
+    const collection = await db.collection("registro_dependency_check");
+    const body = await collection.find().project(campos)
+    //.limit(10)
+    .toArray();
+    const response = {
+        statusCode: 200,
+        body
+    };
+    return response;
 }
